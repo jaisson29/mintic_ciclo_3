47defcf3e6bcea7b6d1f35707881fe83a4dcdb71
Jaisson <jaissonvalbuena29@outlook.com>
2022-09-08 21:01:58 -0500

    introduction-hibernate: Update cfg.xml, add src/class mascotas

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,13 +1,43 @@
 package com.hibernate.introduction;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.introduction.modelo.Mascota;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        // Crear objeto fabricante de sesiones
+        SessionFactory factory = new Configuration()
+            .configure("cfg.xml")
+            .addAnnotatedClass(Mascota.class)
+            .buildSessionFactory();
+        // Abrir sesion
+        Session session = factory.openSession();
+
+        // Preparar la sesion para realizar transacciones
+        session.beginTransaction();
+
+        //Generar transacciones...
+        try {
+            //CRUD
+
+            //CREATE
+            Mascota mascota = new Mascota("Niño", "Quintero", "Perro", "Chandines", 6, "ninguna");
+            session.persist(mascota);
+            session.getTransaction().commit();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Cerra sesion
+        session.close();
     }
 }
