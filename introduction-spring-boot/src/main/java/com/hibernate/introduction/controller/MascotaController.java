e6460d3ef7099a646c0a6b14b2ba826e90f200e6
Jaisson <jaissonvalbuena29@outlook.com>
2022-09-15 19:25:55 -0500

    introduction-spring-boot: add Get method with params into MascotaController

---
+++ introduction-spring-boot/src/main/java/com/hibernate/introduction/controller/MascotaController.java
@@ -0,0 +1,127 @@
+package com.hibernate.introduction.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hibernate.introduction.model.Mascota;
+
+
+@RestController
+@RequestMapping("/mascotas")
+public class MascotaController {
+    //ATRIBUTOS
+    SessionFactory factory;
+
+    public MascotaController(){
+        // Crear objeto fabricante de sesiones
+        factory = new Configuration()
+        .configure("cfg.xml")
+        .addAnnotatedClass(Mascota.class)
+        .buildSessionFactory();
+    }
+    
+    /* @GetMapping
+    public String holaMundo(){
+    return "Hola mundo utilizando Spring Boot";
+    } */
+
+    public Session createSession(){
+        Session session = factory.openSession();
+        session.beginTransaction();
+        return session;
+    }
+
+    @GetMapping
+    public List<Mascota> getList() throws Exception{
+        Session session = factory.openSession();
+        session.beginTransaction();
+        List<Mascota> mascotas = session.createQuery("FROM Mascota", Mascota.class).list();
+        session.close();
+        return mascotas;
+    }
+
+    @GetMapping("/{id}")
+    public Mascota readById(@PathVariable(name = "id")int id) throws Exception {
+        Session session = factory.openSession();
+        session.beginTransaction();
+        Mascota mascota = session.find(Mascota.class, id);
+        session.close();
+        return mascota;
+    }
+
+    @GetMapping("/fullname")
+    public List<Mascota> getByLastname(@RequestParam String nombre, @RequestParam String apellido) throws Exception {
+        // List<String> mascotas = new ArrayList<>();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        List<Mascota> objMascotas = session.createQuery("FROM Mascota WHERE nombre = :n AND apellido = :ap", Mascota.class)
+        .setParameter("n", nombre)
+        .setParameter("ap", apellido).list();
+        session.close();
+        return objMascotas;
+        // return mascotas;
+    }
+
+
+
+    public void create(String nombre, String apellido, String tipo_mascota, String raza, int edad, String observacion) throws Exception {
+        Session session = factory.openSession();
+        session.beginTransaction();
+        Mascota mascota = new Mascota(nombre, apellido, tipo_mascota, raza, edad, observacion);
+        session.persist(mascota);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
+
+    public List<String> objToString(List<Mascota> objMascotas){
+        List<String> mascotas = new ArrayList<>();
+        for (int i = 0; i< objMascotas.size(); i++) {
+            mascotas.add(objMascotas.get(i).toString());
+        }
+        return mascotas;
+    }
+
+
+    public void update(int id, String nombre, String apellido, String tipo_mascota, String raza, int edad, String observacion) throws Exception{
+        Session session = factory.openSession();
+        session.beginTransaction();
+        Mascota mascota = session.find(Mascota.class, id);
+        // Actualizar objeto
+        mascota.setNombre(nombre);
+        mascota.setApellido(apellido);
+        mascota.setTipo_mascota(tipo_mascota);
+        mascota.setRaza(raza);
+        mascota.setEdad(edad);
+        mascota.setObservacion(observacion);
+        // Realizar actualizacion en la BD
+        session.merge(mascota);
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+    public void deleteService(Mascota mascota){
+        Session session = createSession();
+        // Eliminar
+        session.remove(mascota);
+        session.getTransaction().commit();
+    }
+
+    public void delete(int id) throws Exception{
+        Session session = createSession();
+        Mascota mascota = session.find(Mascota.class, id);
+        deleteService(mascota);
+    }
+
+}
