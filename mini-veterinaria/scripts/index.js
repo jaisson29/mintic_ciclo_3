effee56deab764c0e1d86492cd7cdcf39bf2119e
Jaisson <jaissonvalbuena29@outlook.com>
2022-09-17 11:36:22 -0500

    mini-veterinaria: add create into form.js

--- mini-veterinaria/scripts/index.js
+++ mini-veterinaria/scripts/index.js
@@ -5,8 +5,34 @@ async function get_mascotas(){
     const resp = await fetch(URL_API)
     // Obtener datos de la peticion
     const mascotas = await resp.json()
-
     return mascotas
 }
 
-console.log(get_mascotas())
+function show(mascotas){
+    const tbody = document.getElementById("tbody")
+    let tr_body = ""
+    // Iterar mascotas
+    for (let i = 0; i < mascotas.length; i++) {
+        const obj = mascotas[i]
+        tr_body += `
+            <tr>
+                <td>${obj.id}</td>
+                <td>${obj.nombre}</td>
+                <td>${obj.apellido}</td>
+                <td>${obj.tipo_mascota}</td>
+                <td>${obj.raza}</td>
+                <td>${obj.edad}</td>
+                <td>${obj.observacion}</td>
+            </tr>
+        `
+    }
+    
+    tbody.innerHTML = tr_body
+}
+
+async function main(){
+    const mascotas = await get_mascotas()
+    show(mascotas)
+}
+
+main()
